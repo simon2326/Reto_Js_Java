069edb14c448f2e4ae9a2355b4cb28118e6d3ec5
Simon <simon.correa@upb.edu.co>
2024-04-10 18:12:13 -0500

    Reproductor final

--- js/reproductor.js
+++ js/reproductor.js
@@ -7,28 +7,41 @@ window.onload = () => {
     var buttonPrev = document.querySelector('.button__prev');
     var buttonNext = document.querySelector('.button__next');
     var buttonPause = document.querySelector('.button__pause');
+    var control_volumen = document.getElementById("control_volumen");
+
+    let volumen;
 
     // Datos de las diapositivas
     var slides = [
-        { imageUrl: 'https://images.unsplash.com/photo-1500099817043-86d46000d58f?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          songName: 'Sorry The Movement',
-          albumName: 'Justin Bieber',
-          song: 'https://file-examples.com/storage/feed2327706616bd9a07caa/2017/11/file_example_MP3_700KB.mp3'},
-
-        { imageUrl: 'https://images.unsplash.com/photo-1500099817043-86d46000d58f?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          songName: 'Passenger Let Her Go',
-          albumName: 'Passenger',
+        { imageUrl: 'https://i.scdn.co/image/ab67616d0000b2738863bc11d2aa12b54f5aeb36',
+          songName: 'Too Late',
+          albumName: 'After Hours',
           song: 'https://res.cloudinary.com/dpwnji5mr/video/upload/v1668255106/demo/powerful-beat-121791_ninqyk.mp3'},
 
-        { imageUrl: 'https://images.unsplash.com/photo-1500099817043-86d46000d58f?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          songName: 'Song 3',
-          albumName: 'Passenger',
-          song: 'https://file-examples.com/storage/feed2327706616bd9a07caa/2017/11/file_example_MP3_700KB.mp3'},
+        { imageUrl: 'https://i.scdn.co/image/ab67616d0000b2739e1cfc756886ac782e363d79',
+          songName: 'Let It Happen',
+          albumName: 'Currents',
+          song: 'https://res.cloudinary.com/dpwnji5mr/video/upload/v1668255106/demo/powerful-beat-121791_ninqyk.mp3'},
+
+        { imageUrl: 'https://i.scdn.co/image/ab67616d0000b273f79b1ab5cd74db6a2d7c1fda',
+          songName: 'No Me la Moleste',
+          albumName: 'Playa Saturno',
+          song: 'https://res.cloudinary.com/dpwnji5mr/video/upload/v1668255106/demo/powerful-beat-121791_ninqyk.mp3'},
         
-        { imageUrl: 'https://images.unsplash.com/photo-1500099817043-86d46000d58f?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          songName: 'Song 4',
-          albumName: 'Passenger',
-          song: 'https://res.cloudinary.com/dpwnji5mr/video/upload/v1668255106/demo/powerful-beat-121791_ninqyk.mp3'},  
+        { imageUrl: 'https://i.scdn.co/image/ab67616d0000b273f1aad814a40ec7419c234242',
+          songName: 'Luna',
+          albumName: 'Ferxxocalipsis',
+          song: 'https://res.cloudinary.com/dpwnji5mr/video/upload/v1668255106/demo/powerful-beat-121791_ninqyk.mp3'},
+          
+        { imageUrl: 'https://i.scdn.co/image/ab67616d0000b2733066581d697fbdee4303d685',
+          songName: 'Cry Baby',
+          albumName: 'Wiped Out',
+          song: 'https://res.cloudinary.com/dpwnji5mr/video/upload/v1668255106/demo/powerful-beat-121791_ninqyk.mp3'},
+
+        { imageUrl: 'https://i.scdn.co/image/ab67616d0000b273b1f8da74f225fa1225cdface',
+          songName: '505',
+          albumName: 'Favourite Worst Nightmare',
+          song: 'https://res.cloudinary.com/dpwnji5mr/video/upload/v1668255106/demo/powerful-beat-121791_ninqyk.mp3'},
     ];
 
     // Índice de la diapositiva actual
@@ -52,15 +65,22 @@ window.onload = () => {
     function playButton(){
         if (currentSound.playing()) {
             currentSound.pause();
+
+            if (buttonPause.classList.contains("play")) {
+                buttonPause.classList.remove("play");
+                buttonPause.classList.add("pause");}
+
         } else {
             currentSound.play();
+            buttonPause.classList.remove("pause");
+            buttonPause.classList.add("play");
         }
     }
 
     // Función para actualizar la interfaz de usuario con la diapositiva actual
     function updateSlide() {
         var currentSlide = slides[currentSlideIndex];
-        cover.innerHTML = '<div class="item"><img src="' + currentSlide.imageUrl + '" alt=""></div>';
+        cover.innerHTML = '<div class="item"><img class="img_cover" src="' + currentSlide.imageUrl + '" alt=""></div>';
         title.innerHTML = '<div class="item"><div class="song__name">' + currentSlide.songName + '</div><p class="album__name">' + currentSlide.albumName + '</p></div>';
 
         if (currentSound) {
@@ -68,11 +88,16 @@ window.onload = () => {
         }
 
         currentSound = new Howl({
-            src: [currentSlide.song],
+            src: [currentSlide.song, ], 
+            volume:.2
             });
-
-        //buttonPause.addEventListener('click', playButton);
     }
+
+    function actualizarVolumen(){
+        volumen = evento.target.value;
+        currentSound.volume(volumen);
+      }
+    
 
     // Agregar event listeners a los botones "Prev" y "Next"
     buttonPrev.addEventListener('click', prevSlide);
